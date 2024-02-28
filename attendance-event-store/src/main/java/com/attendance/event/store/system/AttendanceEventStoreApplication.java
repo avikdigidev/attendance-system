3f4b4997299076c20d81ac9aeed7e65386075ac4
prakash shelke <avikdigidev@gmail.com>
2024-02-28 13:17:19 +0530

    forgotten imports

--- attendance-event-store/src/main/java/com/attendance/event/store/system/AttendanceEventStoreApplication.java
+++ attendance-event-store/src/main/java/com/attendance/event/store/system/AttendanceEventStoreApplication.java
@@ -2,7 +2,9 @@ package com.attendance.event.store.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AttendanceEventStoreApplication {
 
